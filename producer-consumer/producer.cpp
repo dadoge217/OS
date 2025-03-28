122727c6c832f86679917932b93392e2e12b4961
dadoge217 <crgrant24@gmail.com>
2025-03-28 16:40:37 -0400

    Update producer.cpp; Changed include for header

--- producer-consumer/producer.cpp
+++ producer-consumer/producer.cpp
@@ -1,15 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <fcntl.h>
-#include <sys/shm.h>
-#include <sys/stat.h>
-#include <sys/mman.h>
-#include <unistd.h>
-#include <semaphore.h>
-#include <pthread.h>
-#include <iostream>
-#include <random>
+#include "header.hpp"
 
 const int SIZE = 4096;
 const char* name = "shared.txt";
