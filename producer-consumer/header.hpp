e1843da0f93cc383eaed648814c5899e23a92bc9
dadoge217 <crgrant24@gmail.com>
2025-03-28 16:39:24 -0400

    Create header.hpp

---
+++ producer-consumer/header.hpp
@@ -0,0 +1,18 @@
+#ifndef HEADER_HPP
+#define HEADER_HPP
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <semaphore.h>
+#include <pthread.h>
+#include <iostream>
+#include <random>
+#include <cstring>
+
+#endif
