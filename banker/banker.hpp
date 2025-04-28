12e42afb529346c060d1f8e083cbaa59e923c3a9
dadoge217 <crgrant24@gmail.com>
2025-04-28 19:01:29 -0400

    Update banker.hpp

--- banker/banker.hpp
+++ banker/banker.hpp
@@ -1,1 +1,19 @@
+// Colin Grant
+// banker.hpp
+// 4/24/25
 
+#ifndef BANKER_HPP
+#define BANKER_HPP
+
+#include <iostream>
+#include <fstream>
+
+#define NUMRESOURCES 3 // The number of different resources
+#define NUMPROCESSES 5 // The number of different processes      
+#define ENDLINE ';' // The end of a list type in the file
+#define ENDLIST ',' // The end of a value in the file
+
+void processTables(char* file, int allocated[][NUMRESOURCES], int max[][NUMRESOURCES], int* available);
+int main(int argc, char* argv[]);
+
+#endif
